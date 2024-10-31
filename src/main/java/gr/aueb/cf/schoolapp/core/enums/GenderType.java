c261bc19cc1b40191ed49b643b2b1967f14bf19a
MichalisFra <mikefrantzeskos@gmail.com>
2024-10-31 19:59:52 +0200

    initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/core/enums/GenderType.java
@@ -0,0 +1,5 @@
+package gr.aueb.cf.schoolapp.core.enums;
+
+public enum GenderType {
+    MALE, FEMALE, OTHER
+}
---
+++ src/main/java/gr/aueb/cf/schoolapp/core/enums/Role.java
@@ -0,0 +1,6 @@
+package gr.aueb.cf.schoolapp.core.enums;
+
+public enum Role {
+   TEACHER, EMPLOYEE, SUPER_ADMIN
+
+}
